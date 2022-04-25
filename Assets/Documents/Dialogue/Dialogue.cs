fb621b878d7a5e8c9d2fa7221dc943173042141e
Cameron Hu <hagaboogaplease@gmail.com>
2022-04-24 19:02:40 -0700

    good character

--- Assets/Documents/Dialogue/Dialogue.cs
+++ Assets/Documents/Dialogue/Dialogue.cs
@@ -2,12 +2,13 @@ using UnityEngine.UIElements;
 
 public class Dialogue : ExplicitVisualTree
 {
+    public readonly VisualElement Arrow;
     public readonly Button Settings;
     public readonly Label SpeakerName, Text;
 
-
     public Dialogue(VisualElement root) : base(root)
     {
+
     }
 
 }
